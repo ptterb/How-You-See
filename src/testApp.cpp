0c601684f3c4cf4d8a456fef8d7bf68903c2ca30
tterb <brettjpeterson@gmail.com>
2013-04-11 17:58:52 -0400

    initial commit

---
+++ src/testApp.cpp
@@ -0,0 +1,79 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+
+}
+
+
+
+
+// Read in the string of chars from arduino plugs
+string testApp::readSerial(){
+    
+}
+
+
+// take the current quadrant's FBO and filter it according to the input from the arduino plug
+void testApp::filterFbo(ofFbo &fbo, string inString){
+    
+}
+
+
+
+
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ src/testApp.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "ofMain.h"
+
+class testApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+        string readSerial();
+        void filterFbo(ofFbo &fbo, string inString);
+
+        ofFbo liveView;
+        ofFbo quad1;
+        ofFbo quad2;
+        ofFbo quad3;
+        ofFbo quad4;
+
+		
+};
